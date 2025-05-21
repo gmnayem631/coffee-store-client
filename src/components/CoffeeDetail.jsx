7aabb3ffc712358e5ed1aefbad5fd6907b74ab4b
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-05-21 19:19:46 +0600

    first commit

---
+++ src/components/CoffeeDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoffeeDetail = () => {
+  return <div></div>;
+};
+
+export default CoffeeDetail;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Home from "./components/Home.jsx";
+import AddCoffee from "./components/AddCoffee.jsx";
+import CoffeeDetail from "./components/CoffeeDetail.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/addCoffee",
+        Component: AddCoffee,
+      },
+      {
+        path: "/coffeeDetail",
+        Component: CoffeeDetail,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
